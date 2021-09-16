ffa635e7f20cd344738d2adb5b9ef15d820b1b06
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 01:49:03 +0000

    Create compartir.php

---
+++ include/compartir.php
@@ -0,0 +1,26 @@
+<?php 
+echo '<!-- +1 asincrono -->
+<div style="margin:3px;width:290px;" class="g-plusone" data-annotation="inline"></div>
+<script type="text/javascript">
+window.___gcfg = {lang: \'es-419\'};
+(function() 
+{var po = document.createElement("script");
+po.type = "text/javascript"; po.async = true;
+po.src = "https://apis.google.com/js/plusone.js";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(po, s);
+})();
+</script>
+<!-- Facebook I Like asincrono-->
+<div id="fb-root"></div>
+<script type="text/javascript">
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, \'script\', \'facebook-jssdk\'));</script>
+<a href="https://twitter.com/share" style="margin:3px;" class="twitter-share-button" data-via="" data-lang="es">Twittear</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script><br>	';
+?> 
