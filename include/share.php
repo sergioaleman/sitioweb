1a153bec8a789c3dc3a2eb817247fa7f31986616
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 01:50:13 +0000

    Add files via upload

---
+++ include/share.php
@@ -0,0 +1,5 @@
+<?php 
+echo '<h3>S&iacute;guenos en las redes sociales</h3><a href="http://www.facebook.com/sergioaleman.com.ve"><img src="img/logo-facebook.jpg" width="96" height="96" alt="S&iacute;guenos en Facebook" title="S&iacute;guenos en Facebook"></a>
+<a href="https://twitter.com/SergioAlemanGT"><img src="img/logo-twitter.jpg" width="96" height="96" alt="S&iacute;guenos en Twitter" title="S&iacute;guenos en Twitter"></a>
+<a href="https://plus.google.com/+SergioAlemanOficial"><img src="img/logo-google-plus.jpg" width="96" height="96" alt="S&iacute;guenos en Google+" title="S&iacute;guenos en Google+"></a>';
+?>
