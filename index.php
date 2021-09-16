99fc19cbda61535d44a3140a0222a42006c5ef8c
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 01:43:15 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Sergio Aleman</title>
+<meta name="Description" content="Paginas donde encontrar contenido interesante">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">
+<meta name="Robots" content="index, follow">
+<link href="style/estilo.css" rel="stylesheet" type="text/css" media="screen">
+<link rel="shortcut icon" href="/favicon.ico" />
+
+</head> 
+<body>
+<div id="inicio"></div>
+<div id="header">
+<?php    
+    include "include/logo2.php";   
+?>
+<div id="page">
+<h1>Mi sitio web, actualidad y tecnolog&iacute;a</h1>
+<?php    
+    include "include/compartir.php";   
+?>
+
+<br>
+			<!-- Traductor Google -->
+<center>
+<div id="google_translate_element"> 
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script> 
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+       	
+</div></center>
+			<br>
+
+<div class="intro">
+Seventh-day Adventist Clergyman, Blogger, Web Developer, Writer, and Life Coach in Venezuela. Howdy, I’m Sergio. I’m a web developer living in Venezuela. I am a fan of technology, travel, chocolate, and coffee. I’m also interested in web development and programming.
+</div>
+Esta es la p&aacute;gina principal o de inicio de mi sitio web.<br>
+Es la primera que ven los que entran al sitio por primera vez y en todas las p&aacute;ginas restantes hay un enlace que conduce a ella.<br>
+En ella puedo poner enlaces a las otras p&aacute;ginas, im&aacute;genes sugestivas, videos o cualquier otro elemento.<br>
+<img style="margin:12px 0 12px 0;" src="img/hacer-paginas-php.jpg" width="620" height="400" alt="Imagen para el intro" title="Mi sitio web, actualidad y tecnologia"><br>
+
+	
+			<script async src="https://cse.google.com/cse.js?cx=8a381674092a2b6dd"></script>
+<div class="gcse-search"></div>
+			
+			</br>
+
+
+<h2>Enlaces a mis art&iacute;culos</h2>
+<div class="enlaces">
+<a href="articulo1.php">Primer art&iacute;culo de ejemplo</a><br>
+<a href="articulo2.php">Otro art&iacute;culo</a><br>
+<a href="articulo3.php">Otro art&iacute;culo</a><br>
+<a href="articulo4.php">Otro art&iacute;culo</a><br>
+</div>
+
+Puede tener cualquier cantidad de texto, pero se recomienda que sea breve, que no sea necesario desplazarse por ella, que el contenido principal pueda verlo el lector al primer vistazo.<br>
+En la parte inferior del t&iacute;tulo est&aacute;n los botones para compartirla en las principales redes sociales.<br>
+En el extremo inferior deben ir los botones que permitan a los lectores seguirnos en estos sitios sociales.<br>
+<br>
+
+<?php    
+    include "include/share.php";   
+?> 
+
+<?php    
+    include "include/footer.php";   
+?> 
+</div></div>
+</body> 
+</html>
