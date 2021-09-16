bf07eb923db82d79c490d57285dba7f89993abf9
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 01:45:33 +0000

    Add files via upload

---
+++ articulo1.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Primer artículo de ejemplo del nuevo sitio creado</title>
+<meta name="Description" content="Sencilla página que puede servir de ejemplo y base para  a partir de ella crear cualquier número de páginas similares para tu nuevo sitio.">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">
+<meta name="Robots" content="index, follow">
+<link href="style/estilo.css" rel="stylesheet" type="text/css" media="screen">
+</head> 
+<body>
+<div id="inicio"></div>
+<div id="header">
+<?php    
+    include "include/logo1.php";   
+?>
+<div id="page">
+<div id="migaspan">
+<a href="index.php">Mi sitio</a> » 
+<a href="articulo1.php">Pagina de ejemplo</a> 
+</div>
+<h1>Primer artículo de ejemplo del nuevo sitio creado</h1>
+<?php    
+    include "include/compartir.php";   
+?>
+
+<div class="intro">
+Sencilla página que puede servir de ejemplo y base, para a partir de ella crear cualquier número de páginas similares para tu nuevo sitio web.
+</div>
+
+Esta es una sencilla página de ejemplo del sitio web que deseas crear.<br>
+Puedes crear varias como estas simplemente copiándolas y cambiando alguno de sus elementos.<br>
+Primeramente cambia el nombre del nuevo archivo creado.<br>
+A continuación ábrelo en un editor y cambia el título, la descripción y el contenido.<br>
+El texto puedes crearlo previamente en un editor como Microsoft Word, revisar la ortografía y gramática y copiarlo y pegarlo.<br>
+Separa los saltos de líneas con etiquetas &lt;br&gt;<br>
+Agrega imágenes, enlaces dentro del área del contenido.<br>
+<img style="margin:12px 0 12px 0;" src="img/hacer-paginas-php.jpg" width="620" height="400" alt="Imagen para el articulo" title="Primer artículo de ejemplo del nuevo sitio creado"><br>
+Por último crea un enlace en la página "index.php" al nuevo archivo es todo.<br>
+Finalmente agrega una nueva entrada en el archivo sitemap.xml para que los buscadores la indexen correctamente.<br>
+Después de subir los archivos modificados al servidor web, carga la nueva página en tu navegador y compártela en las redes sociales.<br>
+Cualquier duda lee más información detallada en: 
+<a href="http://norfipc.com/web/plantilla-gratis-php-para-crear-sitio-web-internet.php" target="_blank">Plantilla gratis en PHP para crear un sitio web de internet</a>
+<br><br>
+
+<?php    
+    include "include/share.php";   
+?> 
+<?php    
+    include "include/footer.php";   
+?> 
+</div></div>
+</body> 
+</html>
