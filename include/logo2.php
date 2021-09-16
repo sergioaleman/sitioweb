c4fa0e4593675be2b1763f6098b320c1b96e84a0
SERGIO ALEMAN <sergioaleman@yahoo.com>
2021-09-16 01:53:22 +0000

    Add files via upload

---
+++ include/logo2.php
@@ -0,0 +1,3 @@
+<?php 
+echo '<div id="bar"><a style="color:white;text-decoration:none;" href="http://m.sergioaleman.com.ve/index.php"><b>Sergio Aleman</b></a><div style="clear:both;font-size:0.5em;margin:8px 0 -4px 0;color:white;">Actualidad y tecnolog&iacute;a</div></div>';
+?>
